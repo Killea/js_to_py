280c63b84d9065bf4f79eeecac708596515361f6
Hank Wang <killea@gmail.com>
2022-03-29 23:17:28 -0400

    save 2

--- a.js
+++ a.js
@@ -1,21 +1,55 @@
 console.log(1)
 
-const o1 = {} 
+const o1 = {}
 console.log(o1)
 
 
 
-const array1= [[1,2], [3,4]]
+const array1 = [[1, 2], [3, 4]]
 const flatArray1 = array1.flat()
 console.log(flatArray1)
 
 
 
 
-const array2 = [ 1, 2, 3]
+const array2 = [1, 2, 3]
 
-map1 = array2.map( i=> i*2)
+map1 = array2.map(i => i * 2)
 console.log(map1)
 
-filter1 = array2.filter(i => i>1 )
+filter1 = array2.filter(i => i > 1)
 console.log(filter1)
+
+
+
+
+
+
+
+
+
+
+array2.forEach(i => {
+    console.log(i)
+})
+
+
+
+
+
+const obj2 = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+Object.keys(obj2).forEach(o => {
+    console.log(o)
+})
+Object.entries(obj2).forEach(([k, v]) => {
+    console.log(k, v)
+})
+Object.values(obj2).forEach(v => {
+    console.log(v)
+})
+const obj3 = Object.assign({}, {d: 4}, obj2)
+console.log(obj3)
--- b.py
+++ b.py
@@ -16,8 +16,36 @@ print(flatten(list1))
 
 
 list2 = [1, 2, 3]
-map1 = list(map(lambda x: x*2, list2))
+map1 = [ x*2 for x in list2]
 print(map1)
+# map2 = list(map(lambda x: x*2, list2))
+# print(map2)
 
-filter1 = list(filter(lambda x: x>1, list2))
+filter1 = [ x for x in list2 if x > 1]
 print(filter1)
+# filter2 = list(filter(lambda x: x>1, list2))
+# print(filter2)
+
+
+
+
+# [print(x) for x in list2]
+for x in list2:
+    print(x)
+
+
+
+
+
+dict2 = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+}
+for x in dict2.keys():
+    print(x)
+for (k,v) in dict2.items():
+    print(k,v)
+for v in dict2.values():
+    print(v)
+print ( {"d":4} | dict2)

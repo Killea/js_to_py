3ff8ada29af6b5f3e8774eb3c64b39b31e4da9d4
Hank Wang <killea@gmail.com>
2022-03-29 22:49:21 -0400

    save

--- a.js
+++ a.js
@@ -2,3 +2,20 @@ console.log(1)
 
 const o1 = {} 
 console.log(o1)
+
+
+
+const array1= [[1,2], [3,4]]
+const flatArray1 = array1.flat()
+console.log(flatArray1)
+
+
+
+
+const array2 = [ 1, 2, 3]
+
+map1 = array2.map( i=> i*2)
+console.log(map1)
+
+filter1 = array2.filter(i => i>1 )
+console.log(filter1)
--- b.py
+++ b.py
@@ -1,4 +1,23 @@
+from array import array
+
+
 print(1)
 
-o1 = {}
-print(o1)
+d1 = {}
+print(d1)
+
+
+
+list1 = [[1,2], [3,4]]
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+print(flatten(list1))
+
+
+
+list2 = [1, 2, 3]
+map1 = list(map(lambda x: x*2, list2))
+print(map1)
+
+filter1 = list(filter(lambda x: x>1, list2))
+print(filter1)
